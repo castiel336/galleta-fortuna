3f00108e49fbd0508ad22becac98de5ea195216d
John Cueva M <aslafa333@gmail.com>
2024-04-01 23:20:29 -0500

    arreglando imagen

--- src/components/ShowPhrase.jsx
+++ src/components/ShowPhrase.jsx
@@ -2,19 +2,13 @@ import React from 'react'
 
 const ShowPhrase = ({phrase}) => {
   const objStyles = {
-    backgroundImage:`url(../public/fortuna1/rectangle.svg)`
+    backgroundImage:`url(../fortuna1/rectangle.svg)`
   }
   return (
     <div className='text'>
-      <div className='text__img'>  
         <p className='text__phrase'>
         {phrase.phrase}
       </p>
-      </div>
-    
-      {/* <p className='text__aut'>
-        Fuente:{phrase.author}
-      </p> */}
     </div>
   )
 }
